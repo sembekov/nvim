66a2fd349fd7ca5e05a3a67557e6048df86fd4bd
sembekov <76952248+sembekov@users.noreply.github.com>
2024-04-15 02:27:30 +0500

    Update base.lua

--- lua/base.lua
+++ lua/base.lua
@@ -14,7 +14,12 @@ vim.opt.clipboard:append('unnamedplus')
 vim.opt.showmode = false
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+vim.opt.smarttab = true
+vim.o.termguicolors = true
 --vim.cmd('colorscheme tokyonight-night')
 vim.cmd('colorscheme kanagawa')
 vim.opt.laststatus = 2
 vim.loader.enable()
+--vim.cmd('hi Normal guibg=NONE ctermbg=NONE')
+--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
