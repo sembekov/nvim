a8d3d52077a4a9eb3b6a677e0c6e0c575f8b3b97
sembekov <76952248+sembekov@users.noreply.github.com>
2024-04-15 02:27:44 +0500

    Update config.lua

--- lua/config.lua
+++ lua/config.lua
@@ -38,17 +38,17 @@ require("nvim-tree").setup({
 
 
 
-local highlight = {
-  "Whitespace"
-}
-require("ibl").setup {
-    indent = { highlight = highlight, char = "▏" },
-    whitespace = {
-        highlight = highlight,
-        remove_blankline_trail = false,
-    },
-    scope = { enabled = false },
-}
+--local highlight = {
+--  "Whitespace"
+--}
+--require("ibl").setup {
+--    indent = { highlight = highlight, char = "▏" },
+--    whitespace = {
+--        highlight = highlight,
+--        remove_blankline_trail = false,
+--    },
+--    scope = { enabled = false },
+--}
 
 
 
@@ -59,16 +59,18 @@ lsp_zero.on_attach(function(client, bufnr)
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "<leader>fws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>fd", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("n", "<leader>fca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>frr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>frn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
+-- to learn how to use mason.nvim with lsp-zero
+-- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
 require('mason').setup({})
 require('mason-lspconfig').setup({
   ensure_installed = {'tsserver', 'rust_analyzer'},
@@ -84,6 +86,8 @@ require('mason-lspconfig').setup({
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 
+-- this is the function that loads the extra snippets to luasnip
+-- from rafamadriz/friendly-snippets
 require('luasnip.loaders.from_vscode').lazy_load()
 
 cmp.setup({
